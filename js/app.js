cfa268a9e2a6e5a1e9ec9f7d2b8e67866b6c34ce
Chaitanya009 <chaitanya.thakre009@gmail.com>
2017-08-17 13:12:11 +0530

    c

---
+++ js/app.js
@@ -0,0 +1,62 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic','ngRoute','angularLazyImg','ngAnimate'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+  });
+
+
+  // BACK button
+  $ionicPlatform.registerBackButtonAction(function (event) {
+      window.history.back();
+  }, 100);
+
+  //
+
+
+})
+
+
+
+//routing
+app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
+   $routeProvider
+   .when('/',{
+     templateUrl:'eventdetails.html',
+     controller:'eventdetailscontroller'
+   })
+   .when('/eventdetails',{
+   templateUrl:'eventdetails.html',
+   controller:'eventdetailscontroller'
+   })
+   .when('/feedback',{
+   templateUrl:'feedback.html',
+   controller:'feedbackcontroller'
+   })
+   .when('/blog',{
+    templateUrl:'blog.html',
+    controller:'blogcontroller'
+    })
+    .when('/orderdetail',{
+    templateUrl:'orderdetail.html',
+    controller:'orderdetailcontroller'
+    });
+ }]);
---
+++ js/controller.js
@@ -0,0 +1,95 @@
+
+app.controller('eventdetailscontroller', function($scope, $rootScope){
+
+  $scope.pageClass = 'page-trans';
+  //back button
+  $scope.goback = function(){
+     window.history.back();
+  };
+  //toggle btw show more & show less img
+  $scope.toggle1 = true;
+
+  $scope.$watch('toggle1', function(){
+      $scope.toggleText1 = $scope.toggle1 ? 'Show More' : 'Show Less';
+      $scope.imgurl1 = $scope.toggle1 ? 'img/SHOWMORE-01.png' : 'img/SHOWLESS-01.png';
+  })
+  //toggle btw know more & know less img
+  $scope.toggle2 = true;
+
+  $scope.$watch('toggle2', function(){
+      $scope.toggleText2 = $scope.toggle2 ? 'Know more' : 'Know less';
+      $scope.imgurl2 = $scope.toggle2 ? 'img/SHOWMORE-01.png' : 'img/SHOWLESS-01.png';
+  })
+  //toggle btw All reviews & Less reviews
+  $scope.toggle4 = true;
+
+  $scope.$watch('toggle4', function(){
+      $scope.toggleText4 = $scope.toggle4 ? 'All Reviews' : 'Less Reviews';
+      $scope.imgurl4 = $scope.toggle4 ? 'img/SHOWMORE-01.png' : 'img/SHOWLESS-01.png';
+  })
+
+  //map card
+    var mapProp= {
+        center:new google.maps.LatLng(51.508742,-0.120850),
+        zoom:5,
+    };
+    var map=new google.maps.Map(document.getElementById("mapimage"),mapProp);
+    // map marker
+    var marker = new google.maps.Marker({
+       position: new google.maps.LatLng(51.508742,-0.120850),
+       map: map,
+    });
+
+     //get directions
+     $scope.get_dir = function(){
+       console.log("get directions");
+       launchnavigator.navigate("London, UK");
+     };
+
+     var d = angular.element( document.querySelector('#bottomBar'));
+     d.css('display','none');
+     //display buynow bar
+     $scope.dispbottbar = function(){
+       var d = angular.element( document.querySelector('#bottomBar'));
+       d.css('display','block');
+     }
+
+});
+
+//feedback controller
+app.controller('feedbackcontroller', function($scope){
+
+
+
+
+});
+
+//blog controller
+app.controller('blogcontroller', function($scope){
+
+  $('.toggle-menu').bind('click', function(){
+    var r = angular.element( document.querySelector( '.sidebar-menu' ) );
+    r.removeClass('hide-menu');
+    var a = angular.element( document.querySelector( '#back-cover' ) );
+    a.addClass('back-cover');
+  })
+  $('#cancel').bind('click', function(){
+    var r = angular.element( document.querySelector( '#back-cover' ) );
+    r.removeClass('back-cover');
+    var a = angular.element( document.querySelector( '.sidebar-menu' ) );
+    a.addClass('hide-menu');
+  })
+  $('#back-cover').bind('click', function(){
+    var r = angular.element( document.querySelector( '#back-cover' ) );
+    r.removeClass('back-cover');
+    var a = angular.element( document.querySelector( '.sidebar-menu' ) );
+    a.addClass('hide-menu');
+  })
+
+});
+
+app.controller('orderdetailcontroller', function($scope){
+
+
+
+});
